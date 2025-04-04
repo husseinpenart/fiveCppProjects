438938ada9ef2829a86a6be320834e17ac8e6497
husseinpenart <artistpenman1515@gmail.com>
2025-04-04 11:06:41 -0700

    mongodb connection added

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
-﻿
+﻿#include <iostream>
+#include <mongocxx/client.hpp>
+#include <mongocxx/instance.hpp>
+#include <mongocxx/uri.hpp>
+
 int main() {
-  
+    mongocxx::instance instance{}; // فقط یکبار باید مقداردهی شود
+    mongocxx::client client{ mongocxx::uri{"mongodb://localhost:27017"} };
+
+    try {
+        auto db = client["test"];  // به دیتابیس `test` وصل شو
+        std::cout << "✅ Connected to MongoDB successfully!" << std::endl;
+    }
+    catch (const std::exception& e) {
+        std::cerr << "❌ Connection failed: " << e.what() << std::endl;
+    }
 
     return 0;
 }
